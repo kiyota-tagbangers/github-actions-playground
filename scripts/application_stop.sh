bab87f2e6cd97be8e6c06451ff4d32b92e15f9ec
kiyota <kiyota@tagbangers.co.jp>
2023-11-28 21:24:00 +0900

    バージョン情報のファイルを読み取るように修正

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,6 +3,8 @@
 set -e
 now=$(date +'%Y-%m-%d-%H-%M-%S')
 
+rm -f /tmp/run.log
+
 if [ ! -d /appllication ]; then
   echo "application stop (scripts directory)  $now" >> /tmp/run.log
   exit 0
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,8 +3,6 @@
 set -e
 now=$(date +'%Y-%m-%d-%H-%M-%S')
 
-rm -f /tmp/run.log
-
 if [ ! -d /appllication ]; then
   echo "/appllication does not exist" >> /tmp/run.log
   echo "before install (scripts directory)  $now" >> /tmp/run.log
