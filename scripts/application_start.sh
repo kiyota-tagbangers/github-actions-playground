aba7088d768c15804dc6b9a59cd164124eb1c308
kiyota <kiyota@tagbangers.co.jp>
2023-11-28 22:12:08 +0900

    実行ユーザを変えるとエラーとなるため修正

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-ls -l /tmp/run.log >> /tmp/run-log-persission.txt
 
 now=$(date +'%Y-%m-%d-%H-%M-%S')
-echo "application start (scripts directory)  $now" >> /tmp/run.log
+ls -l /tmp/run.log >> /tmp/run-log-persission.txt
+# ファイルの権限を変えられていない
+# [root@ip-10-0-101-233 ~]# cat /tmp/run-log-persission.txt
+# -rw-r--r--. 1 root root 140 Nov 28 13:09 /tmp/run.log
+# echo "application start (scripts directory)  $now" >> /tmp/run.log
+echo "application start (scripts directory)  $now" >> /tmp/application_start.log
 
 # ユーザを切り替えても意味がなかった
 # sudo su - batch-sample
