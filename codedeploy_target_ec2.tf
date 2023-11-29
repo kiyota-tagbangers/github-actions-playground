ebc971af9cf522171a1f4a6d5a813b7ff3764f32
kiyota <kiyota@tagbangers.co.jp>
2023-11-29 12:14:49 +0900

    runas の指定を無くしてみた

--- codedeploy_target_ec2.tf
+++ codedeploy_target_ec2.tf
@@ -139,3 +139,72 @@ module "ec2" {
   EOF
   USERDATA
 }
+#
+## EC2 - Standby 環境
+## https://registry.terraform.io/modules/terraform-aws-modules/ec2-instance/aws/latest
+## /var/run/sampleapp のディレクトリが存在しない
+#module "ec2_standby" {
+#  source  = "terraform-aws-modules/ec2-instance/aws"
+#  version = "5.3.1"
+#
+#  ami                         = data.aws_ami.redhat_linux.image_id
+#  associate_public_ip_address = true
+#  availability_zone           = "ap-northeast-1a"
+#  create_iam_instance_profile = true
+#  iam_role_name               = local.name_tag
+#  iam_role_policies = {
+#    AmazonSSMManagedEC2InstanceDefaultPolicy = "arn:aws:iam::aws:policy/AmazonSSMManagedEC2InstanceDefaultPolicy"
+#    CodeDeployS3Policy                       = aws_iam_policy.codedeploy_s3.arn
+#  }
+#  instance_type = "t3.micro"
+#  name          = local.name_tag
+#  subnet_id     = module.vpc.public_subnets[0]
+#  vpc_security_group_ids = [
+#    aws_security_group.ec2.id,
+#  ]
+#  instance_tags = {
+#    Name   = local.name_tag
+#    Status = "Standby"
+#  }
+#  user_data = <<-USERDATA
+#  #!/bin/bash
+#  # setup ssm-agent
+#  yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+#
+#  # setup awscli
+#  yum install -y python3
+#  yum install -y python3-pip
+#  pip3 install awscli
+#
+#  # user add
+#  useradd batch-sample
+#
+#  # setup codedeploy-agent
+#  yum install -y ruby
+#  curl https://aws-codedeploy-ap-northeast-1.s3.ap-northeast-1.amazonaws.com/latest/install -O
+#  # https://repost.aws/questions/QUgNz4VGCFSC2TYekM-6GiDQ/dnf-yum-both-fails-while-being-executed-on-instance-bootstrap-on-amazon-linux-2023
+#  # RPM: error: can't create transaction lock on /var/lib/rpm/.rpm.lock (Resource temporarily unavailable)
+#  sleep 30
+#  chmod +x ./install
+#  ./install auto
+#  rm -f ./install
+#
+#  # setup java
+#  yum install -y java
+#
+#  # systemd configuration
+#  cat << EOF > /etc/systemd/system/sampleapp.service
+#  [Unit]
+#  Description=Sample Jar app
+#  After=syslog.target
+#
+#  [Service]
+#  User=root
+#  ExecStart=/bin/java -jar /var/run/sampleapp/app.jar
+#  SuccessExitStatus=143
+#
+#  [Install]
+#  WantedBy=multi-user.target
+#  EOF
+#  USERDATA
+#}
